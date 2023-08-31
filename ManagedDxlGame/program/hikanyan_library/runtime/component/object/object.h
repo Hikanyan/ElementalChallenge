10f77e608cb2f5b6f9b031a43c821ded8a38cbbd
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-08-31 13:36:57 +0900

    [Update]

--- ManagedDxlGame/program/hikanyan_library/runtime/component/object/object.h
+++ ManagedDxlGame/program/hikanyan_library/runtime/component/object/object.h
@@ -2,28 +2,33 @@
 #include <string>
 #include <vector>
 
-class Component; // 前方宣言
+/*
+ *IDの作成・取得
+ *名前の作成
+ *インスタンス化
+ *破棄
+ */
 
 class object {
 public:
-    object() {}
+    object() : id(instantiateID()), name("DefaultName") {}
     virtual ~object() {}
-    object(const std::string& name);
-    void start();
-    void update(float delta_time);
-    void draw();
+    object(const std::string& name) : id(instantiateID()), name(name) {}
 
-    void addComponent(Component* component);
+    virtual void draw();
+    virtual void gameStart();
+    virtual void update(float delta_time);
 
     int getID() const { return id; }
     const std::string& getName() const { return name; }
 
     static object* instantiate(const std::string& name);
     static void destroy(object* object);
-    
+
 protected:
+    int instantiateID();  // メソッドの宣言
     int id;
     std::string name;
-    std::vector<Component*> components;
     static int nextID;
 };
+
--- ManagedDxlGame/program/hikanyan_library/runtime/mono_behaviour.h
+++ ManagedDxlGame/program/hikanyan_library/runtime/mono_behaviour.h
@@ -1,8 +1,10 @@
 ﻿#pragma once
 #include "behaviour.h"
 
-//単一行動するオブジェクトクラス
-//MonoBehaviour は、すべての ひかにゃん スクリプトの派生元となる基本クラスです。
+/*単一行動するオブジェクトクラス
+ *MonoBehaviour は、すべての ひかにゃん スクリプトの派生元となる基本クラスです。
+ *Task,Invoke
+*/
 class mono_behaviour:public behaviour
 {
 public:
