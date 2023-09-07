35932d1892c59393b1b0746587c5a6b5a21af594
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2023-09-07 12:22:29 +0900

    [Update]

--- ManagedDxlGame/program/game/system/user_interface.cpp
+++ ManagedDxlGame/program/game/system/user_interface.cpp
@@ -1,1 +1,9 @@
 ﻿#include "user_interface.h"
+
+
+
+
+void user_interface::draw()
+{
+    
+}
--- ManagedDxlGame/program/game/system/user_interface.h
+++ ManagedDxlGame/program/game/system/user_interface.h
@@ -1,7 +1,26 @@
 ﻿#pragma once
+#include "list.h"
+#include "../dxlib_ext/dxlib_ext.h"
 
 class user_interface
 {
+    int score = 0;
+    float time = 0.0f;
+    std::string help = "ADキーで左右に移動。スペースキーでジャンプ";
+    std::string gameover = "GAME OVER";
+    std::string gameover_help_show = "Rでリスタート";
+    tnl::Vector3 help_show_pos = {100, 600, 0};
+    tnl::Vector3 score_show_pos = {100, 650, 0};
+    tnl::Vector3 gameover_show_pos = {DXE_WINDOW_WIDTH / 2 - 250, DXE_WINDOW_HEIGHT / 2, 0};
+    tnl::Vector3 gameover_help_show_pos = {DXE_WINDOW_WIDTH / 2 - 250, DXE_WINDOW_HEIGHT / 2 + 100, 0};
+    int fontsize = 50;
+
 public:
+    void timer();
+    void addScore(int point);
     
+    void draw();
+    void reset();
 };
+
+extern user_interface user_interface;
